fd3dbb75a64d2cfdc4b791946d72b5d64f0ab9c6
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-10-19 18:38:54 +0000

    Altera headers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,18 @@ class ApplicationDocument extends Document<DocumentProps> {
     return (
       <Html>
         <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Automação para DigitalOcean" />
+          <meta name="keywords" content="digitalocean, automacao, vscodeweb" />
+          <title>DigitalDam</title>
+
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#ffffff" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Roboto:wght@300;400;700&display=swap"
             rel="stylesheet"
