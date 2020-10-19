c9cbb74f81efdc4ffd67f9622e7f2c75a7c0788d
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-10-19 18:36:06 +0000

    Adiciona icones para PWA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,25 @@ import { AppProps } from 'next/app'
 
 import ThemeProvider from '../contexts/ThemeProvider'
 import DigitalOceanTokenProvider from '../contexts/DigitalOceanToken'
+import { Head } from 'next/document'
 
 const Application: NextPage<AppProps> = ({ Component, pageProps }) => (
   <ThemeProvider>
     <DigitalOceanTokenProvider>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Automação para DigitalOcean" />
+        <meta name="keywords" content="digitalocean, automacao, vscodeweb" />
+        <title>DigitalDam</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Component {...pageProps} />
     </DigitalOceanTokenProvider>
   </ThemeProvider>
